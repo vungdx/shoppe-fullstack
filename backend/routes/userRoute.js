bf154c27c0cb01f90c58439fcd23b4e74d6990d3
unknown <vungdoxuankthd@gmail.com>
2020-08-25 14:09:49 +0700

    update frontend

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import User from '../models/userModel'
+import { getToken } from '../util';
+
+const router = express.Router();
+
+router.post("/signin", async (req, res) => {
+    const signinUser = await User.findOne({
+        email: req.body.email,
+        password: req.body.password
+    });
+    if (signinUser) {
+        res.send({
+            _id: signinUser.id,
+            name: signinUser.name,
+            email: signinUser.email,
+            isAdmin: signinUser.isAdmin,
+            token: getToken(signinUser)
+        })
+    } else {
+        res.status(401).send({ msg: 'Invalid email or password' });
+    }
+})
+
+
+
+// Create user is Admin
+router.get("/createAdmin", async (req, res) => {
+    try {
+        const user = new User({
+            name: 'vung',
+            email: 'vungdoxuankthd@gmail.com',
+            password: '12345',
+            isAdmin: false
+        });
+        const newUser = await user.save();
+        res.send(newUser)
+    } catch (error) {
+        res.send({ msg: error.message })
+    }
+})
+
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,42 @@
 import express from 'express';
 import data from './data';
+import dotenv from 'dotenv';
+import config from './config';
+import mongoose from 'mongoose';
+import userRoute from './routes/userRoute'
+
+
+
+dotenv.config();
+const mongodbUrl = config.MONGODB_URL;
+mongoose.connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).catch(error => console.log(error.reason));
+
+
+
 
 const app = express();
+
+// get all products list
 app.get("/api/products", (req, res) => {
     res.send(data.products)
 })
+
+// Create user
+app.use("/api/users", userRoute);
+
+
+// get one product detail
 app.get("/api/products/:id", (req, res) => {
     const productId = req.params.id;
     const product = data.products.find(x => x.id === productId);
     if (product) {
         res.send(product)
     } else {
-        res.status(404).send({msg:"Product Not Found"})
+        res.status(404).send({ msg: "Product Not Found" })
     }
 })
 app.listen(5000, () => {
