1975a33e958ddde54c12ecc7c5f2a890bdc7a1a7
unknown <vungdoxuankthd@gmail.com>
2020-09-03 15:38:39 +0700

    done validate

--- backend/validation.js
+++ backend/validation.js
@@ -4,6 +4,7 @@ const Joi = require("@hapi/joi");
 const registerValidation = (data) => {
     const schema = Joi.object({
         name: Joi.string().alphanum().min(6).required().messages({
+            // Muốn biết type loại gì, thì sang userRoute bỏ message đi xem cả lỗi
             'string.alphanum': `Username không chứa ký tự đặc biệt`,
             'string.min': `Username chứa ít nhất 6 ký tự`,
             'string.empty': `Username không được để trống`,
@@ -16,10 +17,9 @@ const registerValidation = (data) => {
             'string.min': `Password chứa ít nhất 6 ký tự`,
             'string.empty': `Password không được để trống`,
         }),
-        repassword: Joi.string().min(6).required().messages({
-            'string.min': `Repassword chứa ít nhất 6 ký tự`,
-            'string.empty': `Repassword không được để trống`,
-        }),
+        repassword: Joi.any().equal(Joi.ref('password')).messages({
+            'any.only': `Mật khẩu không trùng khớp`
+        })
     });
     return schema.validate(data)
 }
